debf0f7bc980d69fc73f9254e10b4e5089acf53a
phil-gab99 <pgabriel999@hotmail.com>
2021-03-16 01:09:50 -0400

    Updated app launching requirements

--- src/pedigree/Main.java
+++ src/pedigree/Main.java
@@ -11,22 +11,22 @@ public class Main {
     
     /**
      * Retrieves the user passed arguments necessary for beginning the
-     * simulation.
+     * simulation. If no arguments are passed then user must enter arguments
+     * from popping dialog.
      *
      * @param args <ul><li>args[0] is an {@code int} that holds the number of
      * founders for the simulation</li><li>args[1] is a {@code double} that
      * holds the maximum time of the simulation</li></ul>
-     * @throws IllegalArgumentException if not exactly 2 arguments are passed
      */
     
     public static void main(String[] args) {
         
         if (args.length != 2) {
             
-            throw new IllegalArgumentException(
-            "Insufficient arguments (expected 2, got " + args.length + ")");
+            new SimPlot("", "");
+        } else {
+            
+            new SimPlot(args[0], args[1]);
         }
-        
-        new SimPlot(args[0], args[1]);
     }
 }
